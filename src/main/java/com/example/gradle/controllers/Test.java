b8930d87d445a27ea1cab88376ce1cba5518ee2c
dcifuentesc <55813852+dcifuentesc@users.noreply.github.com>
2020-10-07 16:07:56 -0500

    upload project

---
+++ src/main/java/com/example/gradle/controllers/Test.java
@@ -0,0 +1,27 @@
+package com.example.gradle.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+@RequestMapping(path = "api")
+@RestController
+public class Test {
+
+    @GetMapping(path = "/test")
+    public ResponseEntity<String> getPublicIp() throws IOException {
+        URL whatismyip = new URL("http://checkip.amazonaws.com");
+        BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+        String ip = in.readLine();
+        return ResponseEntity.ok().body(ip);
+    }
+
+
+}
+
